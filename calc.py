a16517e31342f688d848c931432eb96b2ed10ee5
gmodh <gearmod93@gmail.com>
2021-04-25 00:10:58 -0500

    Updating Calc

--- calc.py
+++ calc.py
@@ -1,9 +1,5 @@
-fat_wallet = 100
-video_game = 60
-sales_tax = .08 * video_game
-print("I want to buy a game")
-print("how much money do I have? $" + str(fat_wallet))
-video_game += sales_tax
-print("It is going to be $" + str(video_game)) 
-fat_wallet -= video_game
-print("I have " +  str(fat_wallet) + " left")
+print("Hi! I can help you add numbers!")
+num_1 = float(input("What is the first number? : "))
+num_2 = float(input("What is the second number? : "))
+sum = num_1 + num_2
+print("They add up to", sum)
--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-fat_wallet = 100
-video_game = 60
-sales_tax = .08 * video_game
-print("I want to buy a game")
-print("how much money do I have? $" + str(fat_wallet))
-video_game += sales_tax
-print("It is going to be $" + str(video_game)) 
-fat_wallet -= video_game
-print("I have " +  str(fat_wallet) + " left")
+weight = float(input("Enter the weight: "))
+weight = float(input("Enter the weight: "))
