af314f1a28d9fab9cbbf4b70ffa0fd0dbe45d8d3
gmodh <gearmod93@gmail.com>
2023-04-07 21:37:52 -0500

    Chaning Hi to Hello

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-print("Hi! I can help you add numbers!")
+print("Hello! I can help you add numbers!")
 num_1 = float(input("What is the first number? : "))
 num_2 = float(input("What is the second number? : "))
 sum = num_1 + num_2
